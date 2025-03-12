c3f014f22ac65128ee310420e59c324ed74d33df
dpaun <paun_x@ymail.com>
2025-03-12 21:25:49 +0200

    Initial commit

---
+++ src/test/java/com/project/todo_be/TodoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.todo_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TodoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
