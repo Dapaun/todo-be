1159d5e322ddc1c0b8a639cd900d30ba8154fc72
dpaun <paun_x@ymail.com>
2025-03-23 21:58:55 +0200

    Added methods for delete and patch

--- src/main/java/com/project/todo_be/controller/ItemController.java
+++ src/main/java/com/project/todo_be/controller/ItemController.java
@@ -2,10 +2,13 @@ package com.project.todo_be.controller;
 
 import com.project.todo_be.entity.Item;
 import com.project.todo_be.service.ItemService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,4 +32,25 @@ public class ItemController {
     public List<Item> getAllItems() {
         return itemService.getAllItems();
     }
+
+    @DeleteMapping("/item/{id}")
+    public ResponseEntity<?> deleteItem(@PathVariable Long id) {
+        try {
+            itemService.deleteitem(id);
+            return new ResponseEntity<>("Item with id + " + id + " deleted", HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PatchMapping("/item/{id}")
+    public ResponseEntity<?> setToDone(@PathVariable Long id) {
+        Item updateditem = itemService.setToDone(id);
+        log.info("Updated item after service call " + updateditem);
+        if(updateditem == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(updateditem);
+    }
+
 }
--- src/main/java/com/project/todo_be/service/ItemService.java
+++ src/main/java/com/project/todo_be/service/ItemService.java
@@ -2,10 +2,12 @@ package com.project.todo_be.service;
 
 import com.project.todo_be.entity.Item;
 import com.project.todo_be.repository.ItemRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,4 +22,23 @@ public class ItemService {
         return  itemRepository.findAll();
     }
 
+    public void deleteitem(Long id) {
+        if(itemRepository.existsById(id)) {
+            itemRepository.deleteById(id);
+        } else {
+            throw new EntityNotFoundException("Employee with ID " + id + " does not exist");
+        }
+    }
+
+    public Item setToDone(Long id) {
+        Optional<Item> optionalItem = itemRepository.findById(id);
+        if(optionalItem.isPresent()) {
+//            Item existingItem = optionalItem.get();
+//            existingItem.setIsCompleted(true);
+//            return  itemRepository.save(existingItem);
+             optionalItem.get().setIsCompleted(true);
+             return itemRepository.save(optionalItem.get());
+        }
+        return null;
+    }
 }
